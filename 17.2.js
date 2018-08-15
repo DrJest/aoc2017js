5e1477d3ce859b95c0adf1a50a64f435e2f14d98
Simone Albano <dc.jest@gmail.com>
2018-08-15 18:32:08 +0200

    last commit

--- 17.2.js
+++ 17.2.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const start = new Date().getTime();
 const input = 366;
 const rounds = 5e7;
 let pos = 0;
@@ -10,4 +10,6 @@ for( let i = 1; i <= rounds; ++i ) {
 	(pos === 1) && (R = i);
 } 
 
+const end = new Date().getTime();
+console.log( end - start );
 console.log(R);
